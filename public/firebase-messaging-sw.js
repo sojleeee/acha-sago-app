1e4cca3deca9f57eae4c7638c8fa1d207df793bc
sojleeee <65845862+sojleeee@users.noreply.github.com>
2026-08-10 15:26:04 +0900

    Add files via upload

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -26,6 +26,6 @@ messaging.onBackgroundMessage((payload) => {
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow("/admin")
+    clients.openWindow("/")
   );
 });
